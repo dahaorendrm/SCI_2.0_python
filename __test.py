1922d24fee0b00dbce2229dd592b3f82f25c6418
xm home <dahaorendrm@gmail.com>
2021-07-14 16:27:46 -0400

    update __test to view the full result

--- __test.py
+++ __test.py
@@ -1,7 +1,31 @@
 import pickle
+import numpy
+import os
 
-with open('test/test_0_input.pickle', 'rb') as f:
-    inputimgs = pickle.load(f)
+path = 'test/result'
+data_list = os.listdir(path)
+name = '0004'
+for data in data_list:
+    if name in data and 'input' in data:
+        with open(path + '/' + data, 'rb') as f:
+            inputimgs = numpy.load(f)
 
-with open('test/test_0_result.pickle', 'rb') as f:
-    outputimgs = pickle.load(f)
+    if name in data and 'result' in data:
+        with open(path + '/' + data, 'rb') as f:
+            outputimgs = numpy.load(f)
+
+    if name in data and 'gt' in data:
+        with open(path + '/' + data, 'rb') as f:
+            gtimgs = numpy.load(f)
+
+
+from utils import *
+
+inputimgs = np.squeeze(inputimgs)
+display_highdimdatacube(inputimgs)
+
+outputimgs = np.squeeze(outputimgs)
+display_highdimdatacube(outputimgs)
+
+gtimgs = np.squeeze(gtimgs)
+display_highdimdatacube(gtimgs)
