b021ce989f40fac90501a65f2e629370ab579a3a
xm home <dahaorendrm@gmail.com>
2021-10-12 16:20:33 -0400

    update correlation file for new version

--- S2_flow_predict/networks/PWCNet/correlation_package_pytorch1_0/correlation.py
+++ S2_flow_predict/networks/PWCNet/correlation_package_pytorch1_0/correlation.py
@@ -39,20 +39,20 @@ class CorrelationFunction(Function):
             grad_input2 = input2.new()
 
             correlation_cuda.backward(input1, input2, rbot1, rbot2, grad_output, grad_input1, grad_input2,
-                pad_size, kernel_size, max_displacement,stride1, stride2, corr_multiply)
+                int(pad_size), int(kernel_size), int(max_displacement),int(stride1), int(stride2), int(corr_multiply))
 
-        return grad_input1, grad_input2
+        return grad_input1, grad_input2,None,None,None,None,None,None
 
 
 class Correlation(Module):
     def __init__(self, pad_size=0, kernel_size=0, max_displacement=0, stride1=1, stride2=2, corr_multiply=1):
         super(Correlation, self).__init__()
-        self.pad_size = pad_size
-        self.kernel_size = kernel_size
-        self.max_displacement = max_displacement
-        self.stride1 = stride1
-        self.stride2 = stride2
-        self.corr_multiply = corr_multiply
+        self.pad_size = torch.tensor(pad_size)
+        self.kernel_size = torch.tensor(kernel_size)
+        self.max_displacement = torch.tensor(max_displacement)
+        self.stride1 = torch.tensor(stride1)
+        self.stride2 = torch.tensor(stride2)
+        self.corr_multiply = torch.tensor(corr_multiply)
 
     def forward(self, input1, input2):
 
