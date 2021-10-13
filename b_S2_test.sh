b4974c921055d85ac95064705929ded315e720b5
Xiao Ma Cluster <xmdrm@udel.edu>
2021-10-13 11:00:42 -0400

    update batch config

--- b_S2_test.sh
+++ b_S2_test.sh
@@ -3,18 +3,18 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=1
 
-#SBATCH --mem=40G
+#SBATCH --mem=20G
 # SBATCH --mem-per-cpu=10G
 
-#SBATCH --job-name=chasti_S1_test
+#SBATCH --job-name=chasti_S2_test
 # SBATCH --partition=_workgroup_
-# SBATCH --partition=standard
-#SBATCH --partition=devel
-#SBATCH --gres=gpu:t4:1
+#SBATCH --partition=standard
+# SBATCH --partition=devel
+#SBATCH --gres=gpu:t4
 
-#SBATCH --time=0-1:00:00
+#SBATCH --time=0-01:00:00
 # SBATCH --output=ArraySCI%A-%a.out
 #SBATCH --mail-user='xmdrm@udel.edu'
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT_90
@@ -27,4 +27,4 @@ python3 -u test.py
 cd ..
 mkdir -p S3_spectra_convert/data/test
 cp -r S2_denoiser/result/*.npz S3_spectra_convert/data/test
-
+nvidia-smi
