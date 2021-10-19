6803a30369f29998f2e26274b56efd991fa70a4d
Xiao Ma Cluster <xmdrm@udel.edu>
2021-10-19 11:51:31 -0400

    save in temp now suport 3D data

--- S2_flow_predict/test.py
+++ S2_flow_predict/test.py
@@ -8,13 +8,23 @@ import itertools
 def saveintemp(data,name='test'):
     if not os.path.exists('temp'):
         os.makedirs('temp')
-    for idx in itertools.product(list(range(data.shape[2])),list(range(data.shape[3]))):
-        # print(idx)
-        im1 = data[:,:,idx[0],idx[1]]
-        im1 = np.round(im1 * 255.0)
-        im1 = Image.fromarray(im1)
-        im1 = im1.convert("L")
-        im1 = im1.save(f"temp/{name}_{idx}.jpg")
+    if data.ndim == 3:
+        for idx in range(data.shape[2]):
+            # print(idx)
+            im1 = data[:,:,idx]
+            im1 = np.round(im1 * 255.0)
+            im1 = Image.fromarray(im1)
+            im1 = im1.convert("L")
+            im1 = im1.save(f"temp/{name}_{idx}.jpg")
+    elif data.ndim == 4:
+        for idx in itertools.product(list(range(data.shape[2])),list(range(data.shape[3]))):
+            # print(idx)
+            im1 = data[:,:,idx[0],idx[1]]
+            im1 = np.round(im1 * 255.0)
+            im1 = Image.fromarray(im1)
+            im1 = im1.convert("L")
+            im1 = im1.save(f"temp/{name}_{idx}.jpg")
+  
 
 def outputevalarray(data,ref):
     v_psnr = []
