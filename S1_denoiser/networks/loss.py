9256cbab516415d4343793919b9384a3540752fb
xm home <dahaorendrm@gmail.com>
2022-03-01 20:01:02 -0500

    update loss with edge detection

--- S1_denoiser/networks/loss.py
+++ S1_denoiser/networks/loss.py
@@ -1,6 +1,25 @@
 import torch
 import numpy as np
 
+class Sobel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.filter = nn.Conv2d(in_channels=1, out_channels=2, kernel_size=3, stride=1, padding=0, bias=False)
+
+        Gx = torch.tensor([[2.0, 0.0, -2.0], [4.0, 0.0, -4.0], [2.0, 0.0, -2.0]])
+        Gy = torch.tensor([[2.0, 4.0, 2.0], [0.0, 0.0, 0.0], [-2.0, -4.0, -2.0]])
+        G = torch.cat([Gx.unsqueeze(0), Gy.unsqueeze(0)], 0)
+        G = G.unsqueeze(1)
+        self.filter.weight = nn.Parameter(G, requires_grad=False)
+
+    def forward(self, img):
+        x = self.filter(img)
+        x = torch.mul(x, x)
+        x = torch.sum(x, dim=1, keepdim=True)
+        x = torch.sqrt(x)
+        return x
+
+
 class XEDiceLoss(torch.nn.Module):
     """
     Computes (0.5 * CrossEntropyLoss) + (0.5 * DiceLoss).
@@ -9,6 +28,7 @@ class XEDiceLoss(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.xe = torch.nn.MSELoss()
+        self.edge = Sobel()
 
     def forward(self, pred, true):
         #valid_pixel_mask = true.ne(255)  # valid pixel mask
@@ -16,6 +36,8 @@ class XEDiceLoss(torch.nn.Module):
         # Cross-entropy loss
         #temp_true = torch.where((true == 255), 0, true)  # cast 255 to 0 temporarily
         #print(f'pred max is {pred.max()}, true max is {true.max()}')
+        maps = self.edge(true)
+        ed_loss = self.xe(pred*maps, true*maps)
         xe_loss = self.xe(pred, true)
         #xe_loss = xe_loss.mean()
 
@@ -25,7 +47,7 @@ class XEDiceLoss(torch.nn.Module):
         # true = true.masked_select(valid_pixel_mask)
         # dice_loss = 1 - (2.0 * torch.sum(pred * true)) / (torch.sum(pred + true) + 1e-7)
 
-        return xe_loss
+        return xe_loss+0.5*ed_loss
 
 
 def intersection_and_union(pred, true):
