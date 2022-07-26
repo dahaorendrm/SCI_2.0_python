ee121252dfb2864f46f4590635dc57417e64dcc3
xm home <dahaorendrm@gmail.com>
2022-07-25 21:48:40 -0400

    second set of data

--- exp_datapreprocess.py
+++ exp_datapreprocess.py
@@ -45,8 +45,47 @@ if __name__=='__main__':
     # for idx,img in enumerate(imgs):
     #     scio.savemat(f'expdata/Mango2_{idx:04d}.mat',{'img':img})
     ########################################################################
+    # ## 20220725data
+    # savepath = Path('expdata20220723')
+    # if not os.path.exists(savepath):
+    #     os.mkdir(savepath)
+    # path = Path('/lustre/arce/X_MA/data/expdata_SCI_2.0/20220723data')
+    # shift = (3,0)
+    # mask = converter(path/"calihi",(213,236),1024,shift,0.25)
+    # mask = np.stack(mask,2)
+    # utils.saveintemp(mask,'expmask')
+    # scio.savemat(savepath/'mask.mat',{'mask':mask})
+    #
+    # imgs = converter(path/'lego1_',(317,356),1024,shift,0.25)
+    # for idx,img in enumerate(imgs):
+    #     scio.savemat(savepath/f'lego1_{idx:04d}.mat',{'img':img})
+    #
+    # imgs = converter(path/'lego2_lowexp',(357,396),1024,shift,0.25)
+    # for idx,img in enumerate(imgs):
+    #     scio.savemat(savepath/f'lego2_lowexp{idx:04d}.mat',{'img':img})
+    #
+    # imgs = converter(path/'mario',(798,837),1024,shift,0.25)
+    # for idx,img in enumerate(imgs):
+    #     scio.savemat(savepath/f'mario{idx:04d}.mat',{'img':img})
+    #
+    # imgs = converter(path/'lego_slow',(397,436),1024,shift,0.25)
+    # for idx,img in enumerate(imgs):
+    #     scio.savemat(savepath/f'lego_slow{idx:04d}.mat',{'img':img})
+    #
+    # imgs = converter(path/'lego_arm',(838,877),1024,shift,0.25)
+    # for idx,img in enumerate(imgs):
+    #     scio.savemat(savepath/f'lego_arm{idx:04d}.mat',{'img':img})
+    #
+    # imgs = converter(path/'116fps_toys_high_motion',(1078,1197),1024,shift,0.25)
+    # for idx,img in enumerate(imgs):
+    #     scio.savemat(savepath/f'116fps_toys_high_motion{idx:04d}.mat',{'img':img})
+    #
+    # imgs = converter(path/'text_still',(558,559),1024,shift,0.25)
+    # for idx,img in enumerate(imgs):
+    #     scio.savemat(savepath/f'text_still{idx:04d}.mat',{'img':img})
+
     ## 20220725data
-    savepath = Path('expdata20220725_preprocessed')
+    savepath = Path('expdata20220723_2')
     if not os.path.exists(savepath):
         os.mkdir(savepath)
     path = Path('/lustre/arce/X_MA/data/expdata_SCI_2.0/20220723data')
@@ -56,30 +95,10 @@ if __name__=='__main__':
     utils.saveintemp(mask,'expmask')
     scio.savemat(savepath/'mask.mat',{'mask':mask})
 
-    imgs = converter(path/'lego1_',(317,356),1024,shift,0.25)
+    imgs = converter(path/'text',(597,636),1024,shift,0.25)
     for idx,img in enumerate(imgs):
-        scio.savemat(savepath/f'lego1_{idx:04d}.mat',{'img':img})
+        scio.savemat(savepath/f'text{idx:04d}.mat',{'img':img})
 
-    imgs = converter(path/'lego2_lowexp',(357,396),1024,shift,0.25)
+    imgs = converter(path/'116fps_toys',(958,1077),1024,shift,0.25)
     for idx,img in enumerate(imgs):
-        scio.savemat(savepath/f'lego2_lowexp{idx:04d}.mat',{'img':img})
-
-    imgs = converter(path/'mario',(798,837),1024,shift,0.25)
-    for idx,img in enumerate(imgs):
-        scio.savemat(savepath/f'mario{idx:04d}.mat',{'img':img})
-
-    imgs = converter(path/'lego_slow',(397,436),1024,shift,0.25)
-    for idx,img in enumerate(imgs):
-        scio.savemat(savepath/f'lego_slow{idx:04d}.mat',{'img':img})
-
-    imgs = converter(path/'lego_arm',(838,877),1024,shift,0.25)
-    for idx,img in enumerate(imgs):
-        scio.savemat(savepath/f'lego_arm{idx:04d}.mat',{'img':img})
-
-    imgs = converter(path/'116fps_toys_high_motion',(1078,1197),1024,shift,0.25)
-    for idx,img in enumerate(imgs):
-        scio.savemat(savepath/f'116fps_toys_high_motion{idx:04d}.mat',{'img':img})
-
-    imgs = converter(path/'text_still',(558,559),1024,shift,0.25)
-    for idx,img in enumerate(imgs):
-        scio.savemat(savepath/f'text_still{idx:04d}.mat',{'img':img})
+        scio.savemat(savepath/f'116fps_toys{idx:04d}.mat',{'img':img})
