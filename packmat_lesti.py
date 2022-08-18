f927620bc76d6b1c3a14fd2f94f844ba7cab4b89
xm home <dahaorendrm@gmail.com>
2022-08-18 17:53:44 -0400

    pack exp data

--- packmat_lesti.py
+++ packmat_lesti.py
@@ -41,3 +41,34 @@ led_curve = signal.resample(led_curve,8,axis=0)
 
 scio.savemat(savepath/'4D_Lego_24.mat',{'DATASET_NAME':'4D_Lego_24', 'MODEL':'LESTI',
     'LEDmodul':led_curve, 'mea':mea, 'IND':1,'modul':modul})
+
+##################################################################################################
+
+mea = tifffile.imread('/lustre/arce/X_MA/SCI_2.0_python/resultpaper/S0/spvi/mea/mario0010.tiff')
+mea = mea/np.amax(mea)*3
+mask = scio.loadmat('./expdata20220723/mask.mat')['mask']
+mask = mask/np.amax(mask)
+
+led_curve = scio.loadmat('/lustre/arce/X_MA/SCI_2.0_python/S0_gaptv/BandsLed.mat')['BandsLed']
+led_curve = led_curve[4:-2,:]
+
+
+nled = led_curve.shape[1]
+nf = 3
+mask_ = np.expand_dims(np.reshape(mask[:,:,:nled*nf],(256,256,nled,nf)),axis=2)
+modul = mask_ * np.expand_dims(led_curve,axis=2)  # Shape:nr,nc,nl,nled,nf
+nr=256
+nc=256
+nl=led_curve.shape[0]
+modul = np.sum(modul,axis=3) # Shape:nr,nc,nl,nf
+modul = np.swapaxes(modul,2,3)
+modul = np.reshape(modul,(nr*nc*nf,nl))
+modul = signal.resample(modul,8,axis=1)
+modul = np.reshape(modul,(nr,nc,nf,nled))
+modul = np.swapaxes(modul,2,3)
+led_curve = signal.resample(led_curve,8,axis=0)
+
+
+
+scio.savemat(savepath/'mario.mat',{'DATASET_NAME':'mario', 'MODEL':'LESTI',
+    'LEDmodul':led_curve, 'mea':mea, 'IND':1,'modul':modul})
