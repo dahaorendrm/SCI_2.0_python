c81a93c769652d5521f716cb2f3bf547500a61d6
xm home <dahaorendrm@gmail.com>
2023-03-23 07:56:00 -0700

    save s2 results in pickle in temp

--- S2_flow_predict/motion.py
+++ S2_flow_predict/motion.py
@@ -89,12 +89,13 @@ class Motion:
             input = torch.from_numpy(input).to(self.device)
             output = self.model(input)
         output = output.cpu().numpy()
-        # with open("S2_result/dainflow2_results.pickle",'wb') as f:
-        #     pickle.dump(output,f)
+        with open("temp/dainflow2_results.pickle",'wb') as f:
+            pickle.dump(output,f)
         # with open("S2_result/dainflow2_results_ref.pickle",'wb') as f:
         #     pickle.dump(origs,f)
         self.psnr = []
         self.ssim = []
+        output = output[0]
         if origs is not None:
             print('output_images shape'+repr(output.shape))
             print('orig shape'+ repr(origs.shape))
