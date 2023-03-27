e4471e74e745ae7b8a864099430060a969dcb6d2
xm home <dahaorendrm@gmail.com>
2023-03-27 10:54:44 -0700

    add new batch file

---
+++ b_peerreview_motionmap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+
+#SBATCH --cpus-per-task=10
+
+#SBATCH --mem=80G
+# SBATCH --mem-per-cpu=10G
+
+#SBATCH --job-name=lesti_review
+#SBATCH --partition=idle
+
+#SBATCH --time=0-10:00:00
+# SBATCH --output=ArraySCI%A-%a.out
+#SBATCH --mail-user='xmdrm@udel.edu'
+#SBATCH --mail-type=END,FAIL,TIME_LIMIT_90
+#SBATCH --gpus=1
+# SBATCH --array=4-10
+
+# export OMP_NUM_THREADS=4
+vpkg_require xm_pytorch/20210806-LESTI_2.0_DAIN
+python3 -u sim_test.py
+
+
